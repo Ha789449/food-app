ab6ce1503c328470f84afcd6e9d7e77c7b93e39e
Abdul Hakeem <hakeemabd638@gmail.com>
2024-09-01 01:40:45 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import Headlinecarts from "./Components/Headlinecarts"
+import Food from "./Components/Food"
+import Category from "./Components/Category"
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Headlinecarts/>
+      <Food/>
+      <Category/>
+      </div>
+    </>
+  )
+}
+
+export default App
